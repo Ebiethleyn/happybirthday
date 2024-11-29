5be3b0dc6ace443f2c41e8c5f4262ef399a66356
Gabriel Daten Leyn <ebiethslank763@gmail.com>
2024-11-29 19:30:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  const button = document.querySelector('.cta-button');
+  button.addEventListener('click', () => {
+    alert('Time to celebrate! 🎉 Enjoy your day!');
+  });
+};
